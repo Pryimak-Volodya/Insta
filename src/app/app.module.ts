462762e404548ff7547a156a41f0b314e17512ce
Pryimak-Volodya <kikimora11%08>
2020-11-13 12:26:33 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Candy1Component } from './candy1/candy1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Candy1Component
   ],
   imports: [
     BrowserModule
